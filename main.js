806fee8e24726a2c8891424e11b0d83e7ed57837
Brian Espindola <briianskt@gmail.com>
2023-08-19 23:53:03 -0300

    detalles de productos

--- main.js
+++ main.js
@@ -3,87 +3,76 @@
 //Molde creador de productos
 
 class Producto {
-       constructor(nombre, precio, imagen, descripcion, cantidad) {
-         this.nombre = nombre;
-         this.precio = precio;
-         this.imagen = imagen;
-         this.descripcion = descripcion;
-         this.vendido = false;
-       }
-    
-       vender() {
-         this.vendido = true;
-       }
-     }
-//productos en el mercado
-
- const producto1 = new Producto(
-   "cadenita corazon",
-   200,
-   "img src=../imagenes/cadenita_corazon.JPG",
-   "collar de oro 18k",
-   2,
-    );
-
- const producto2 = new Producto(
-   "cadenita sol y luna",
-   200,
-   "img src=../imagenes/cadenitas_s_l.JPG",
-   "collar de oro 18k",
-   2,
- );
-
- const producto3 = new Producto(
+    constructor(nombre, precio, imagen, descripcion, cantidad) {
+      this.nombre = nombre;
+      this.precio = precio;
+      this.imagen = imagen;
+      this.descripcion = descripcion;
+      this.vendido = false;
+    }
+  }
+  //productos en el mercado
+  
+  const producto1 = new Producto(
+    "cadenita corazon",
+    200,
+    "img src=../imagenes/cadenita_corazon.JPG",
+    "collar de oro 18k",
+    2,
+  );
+  
+  const producto2 = new Producto(
+    "cadenita sol y luna",
+    200,
+    "img src=../imagenes/cadenitas_s_l.JPG",
+    "collar de oro 18k",
+    2,
+  );
+  
+  const producto3 = new Producto(
     "cadenita oro",
     200,
     "src=../imagenes/cadenita_enchapada.JPG",
     "cadenita enchapada en oro",
     2,
- );
-
- const producto4 = new Producto(
+  );
+  
+  const producto4 = new Producto(
     "aritos",
     200,
     "src=../imagenes/aritos_enchapados.JPG",
     "aritos de oro 18k",
     2,
- );
-
-
-//array con productos
-
-const arrayProducto = [producto1, producto2, producto3, producto4,];
-
- let nombre = prompt("Ingrese el nombre del producto  consultar");
-
-while (nombre != "ESC") {
-  let producto;
-  for (const item of productos) {
-    if (item.nombre === nombre) {
-      producto = item;
+  );
+  
+  
+  //array con productos
+  
+  const arrayProducto = [producto1, producto2, producto3, producto4,];
+  
+  function consultarProductos(){
+  
+    let nombre = prompt("Ingrese el nombre del producto  consultar, para salir ingresar ESC");
+  
+    while (nombre != "ESC") {
+      const producto = arrayProducto.find(item => item.nombre === nombre);
+    
+      if (producto) {
+        console.log(
+        `
+        Nombre: ${producto.nombre}
+        Imagen: ${producto.imagen}
+        Precio: $${producto.precio}
+        Descripcion: ${producto.descripcion}
+        Cantidad: ${producto.cantidad}
+        `
+        );
+        break
+      } else{
+        console.log(`Producto no disponible`);
+        break
+      }
     }
   }
-
-  if (producto) {
-    let mensaje = `
-      Nombre: ${producto.nombre}
-      Imagen: ${producto.imagen}
-      Precio: $${producto.precio}
-      Descripcion: ${producto.descripcion}
-      Cantidad: ${producto.cantidad}
-    `;
-
-    alert(mensaje);
-  } else {
-    alert("El producto no se encuentra disponible");
-  }
-
-  nombre = prompt("Ingrese el nombre del producto a  consultar");
-}
-
-vender()
-    if (this.cantidad === 0) {
-      console.log("No quedan productos");
-    } else {
-      this.cantidad -= 1;
-    }
+  
+  consultarProductos()
